a55b48b149f5793429e10d44f01f276bc1707d43
allandrp <allandrifqy77@gmail.com>
2022-05-13 17:14:32 +0700

    layout: creating discussion and detail discussion layout

---
+++ app/src/main/java/com/example/psycareapp/DetailDiscussionActivity.kt
@@ -0,0 +1,11 @@
+package com.example.psycareapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailDiscussionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_discussion)
+    }
+}
---
+++ app/src/main/java/com/example/psycareapp/DiscussionActivity.kt
@@ -0,0 +1,11 @@
+package com.example.psycareapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DiscussionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_discussion)
+    }
+}
