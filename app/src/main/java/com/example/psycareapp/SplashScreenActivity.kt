e558770d0d7cd40e5cf1cac38a367c35d8ca978a
Wahid28 <71857145+Wahid28@users.noreply.github.com>
2022-05-18 19:20:07 +0700

    Menambahkan Splash Screen

---
+++ app/src/main/java/com/example/psycareapp/SplashScreenActivity.kt
@@ -0,0 +1,26 @@
+package com.example.psycareapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+import android.view.WindowManager.LayoutParams.*
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        window.setFlags(
+            FLAG_FULLSCREEN,
+            FLAG_FULLSCREEN
+        )
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
